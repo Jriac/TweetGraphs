8f91b3c5e62454cd06173dc63f9cdb6539228dbb
Xavier Cervilla Martinez <x.cervilla@gmail.com>
2015-07-10 17:56:50 +0200

    Version 0.1

---
+++ app/UserTags.php
@@ -0,0 +1,9 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserTags extends Model {
+
+	protected $table = "usertags";
+
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+@extends('app')
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+			<div class="panel panel-default">
+				<div class="panel-heading">Home</div>
+
+				<div class="panel-body">
+					You are logged in!
+				</div>
+				<div class="panel-heading">#bacalaodebilbao</div>
+
+				<div class="panel-body">
+					Stats
+				</div>
+				<div class="panel-heading">#developers</div>
+
+				<div class="panel-body">
+					Stats
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
