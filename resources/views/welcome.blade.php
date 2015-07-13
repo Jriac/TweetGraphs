6a0c82f169bd71455c1475d3fb8981fa38086e44
Xavier Cervilla Martinez <x.cervilla@gmail.com>
2015-07-13 10:01:17 +0200

    no message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,8 +38,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
+				<h1 class="title">Hi!</h1>
 			</div>
 		</div>
 	</body>
