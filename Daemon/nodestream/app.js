e6ce34707688735d13373271eb9da86095b2b9c0
Justin Rios <Jriac91@gmail.com>
2015-07-21 23:30:41 +0200

    Guarda en mongo y pilla los trends que aun falta filtrar y convertir en array

--- Daemon/nodestream/app.js
+++ Daemon/nodestream/app.js
@@ -1,28 +1,20 @@
 var moment = require('moment')
     , twitter = require('twitter')
     , common = require('./keys')
-    ,mongodb=require('mongodb')
     , globalStream;
 
-var MongoClient = require('./').MongoClient;
+var MongoClient = require('mongodb').MongoClient
+    , assert = require('assert');
 
-function connect_to_mongo(callback) {
-    if (database != null) {
-        callback(null, database);
-    } else {
-        var connection = "mongodb://127.0.0.1:27017/tweetgraphs";
-        MongoClient.connect(connection, {
-            server : {
-                reconnectTries : 5,
-                reconnectInterval: 1000,
-                autoReconnect : true
-            }
-        }, function (err, db) {
-            database = db;
-            callback(err, db);
-        });
-    }
-}
+// Connection URL
+
+
+
+
+
+
+
+///conexion a mongodb
 var twit = new twitter({
     consumer_key: common.keys.consumer_key,
     consumer_secret: common.keys.consumer_secret,
@@ -30,7 +22,11 @@ var twit = new twitter({
     access_token_secret: common.keys.access_token_secret
 });
 
-var connectStream = function(string){
+var connectStream = function(string,db){
+
+
+        // db.close();
+
     twit.stream('statuses/filter', {track: string},  function(stream) {
         globalStream = stream;
 
@@ -42,8 +38,10 @@ var connectStream = function(string){
         stream.on('data', function (aTweet) {
             var myId = aTweet.id_str;
             aTweet.created_at = moment(aTweet.created_at);
-            console.info( JSON.stringify(aTweet, null, 2) );
+           // console.info( JSON.stringify(aTweet, null, 2) );
             //guardar en mongo
+            console.info("insertando....");
+            insertDocuments(db,aTweet);
         });
 
         stream.on('destroy', function (response) {
@@ -52,24 +50,58 @@ var connectStream = function(string){
 
     });
 
+
+        var insertDocuments = function (db, data) {
+            // Get the documents collection
+            var collection = db.collection('tweets');
+            // Insert some documents
+            collection.insert(data, function (err, result) {
+
+                assert.equal(err, null);
+                assert.equal(1, result.result.n);
+                assert.equal(1, result.ops.length);
+                console.log("Inserted document into the document collection");
+                console.log("inserted");
+            });
+        }
+
+
+
+
+
 };
 
 var disconnectStream = function(){
     globalStream.destroy();
 };
+var url = 'mongodb://localhost:27017/tweetgraphs';
+MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
 
 setInterval(function(){
 globalStream.destroy();
     twit.get('trends/place.json', {id : 1}, function(err, data){
         console.info(JSON.stringify(data, null, 2));
         //array de tags
-        connectStream();
+        hashtagsTrends= JSON.parse(JSON.stringify(data));
+        console.log("Todos los datos recibidos");
+        console.info(hashtagsTrends);
+        trends=hashtagsTrends[0].trends;
+        console.info("Solo los trends")
+        console.log(trends);
+
+        console.info("empezando nuevo stream despues de los tags...");
+        connectStream("developer,css,",db);
 
     });
 
 
 
+}, 10*1000);
 
-}, 5*60*1000);
 
-connectStream("default");
+connectStream("default",db);
+
+
+});
