2e2110682357f169e77ab5c23d7b9f8bfc77343a
Sergi Soler <jojomasira@gmail.com>
2015-07-22 16:18:29 +0200

    Em sembla que ja pot rebre el Json

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,13 @@
-angular.module('app').controller(
-    'MainCtrl',
-    [
-        '$scope',
-        '$http',
-        '$ItemResource',
-        '$nombre_modelo',
-        function($scope, $http, ItemResource, nombre_modelo) {
-            $scope.tags = ItemResource.items;
-            ItemResource.getItems();
-    
-            $scope.loadTags = function(query){
-                return $http.get('v1/tag');
-            };
-            
-            $scope.envia = function(str_name){
-                nombre_modelo.CrearTag(str_name);
-            };
-            
-            $scope.elimina = function(str_name){
-                console.log('sha creat eliminat' + str_name);
-            nombre_modelo.EliminarTag(str_name);
-            };
+var app = angular.module('plunker', ['ngTagsInput']);
+
+app.controller('MainCtrl', function($scope, $http) {
+    $.ajax({
+        url: '/v1/user/tags',
+        success: function(response) {
+            jsonData = response;
         }
-    ]
-);
+    });
+    
+    
+  $scope.tags = jsonData;
+});
