5081e4e3df635646c57b17ba24f8aa716fc6144d
Justin Rios <Jriac91@gmail.com>
2015-07-22 16:53:20 +0200

    daemon "terminado"

--- Daemon/nodestream/app.js
+++ Daemon/nodestream/app.js
@@ -2,6 +2,8 @@ var moment = require('moment')
     , twitter = require('twitter')
     , common = require('./keys')
     , globalStream;
+var redis = require('redis');
+var client = redis.createClient();
 
 var MongoClient = require('mongodb').MongoClient
     , assert = require('assert');
@@ -27,12 +29,15 @@ var connectStream = function(string,db){
 
         // db.close();
 
+    console.info("AHORA VIENE EL  STRING QUE HACE EL STREAMING!!!!!!!!!!!!!!!!!!!!!!!!");
+    console.log(string);
     twit.stream('statuses/filter', {track: string},  function(stream) {
         globalStream = stream;
 
         stream.on('error', function(a,b){
             console.error(a);
             console.error(b);
+            console.error("Ha llegado datos de error");
         });
 
         stream.on('data', function (aTweet) {
@@ -86,30 +91,36 @@ function trackTrends(woeids) {
     for(var k = 0, len1=woeids.length; k < len1 ; k++ ){
 
       twit.get('trends/place.json', {id: woeids[k]}, function (err, data) {
-       console.info(JSON.stringify(data));
+       //console.info(JSON.stringify(data));
         //array de tags
         hashtagsTrends = JSON.parse(JSON.stringify(data));
         //console.log("Todos los datos recibidos");
-       // console.info(hashtagsTrends);
+        // console.info(hashtagsTrends);
         trends = hashtagsTrends[0].trends;
         //console.info("Solo los trends")
-       // console.log(trends[0].name);
-            console.log("usando...");
-            console.log(k);
-            console.log(len1);
-          console.log("Linea 100");
+        //console.log(trends);
+        // console.log("usando...");
+        //  console.log(k);
+        // console.log(len1);
+        // console.log("Linea 100");
 
         for (var i = 0, len = trends.length; i < len; i++) {
-            console.log(trends[i].name);
+           // console.log(trends[i].name);
             palabras.push(trends[i].name);
         }
-        console.log("hashes en la funcion tracktrends");
-        console.log(palabras.join(","));
+        console.log("palabras entrando en redis....");
+          //console.log(palabras.join(","));
+          client.set('trends', palabras.join(","), function(err, reply) {
+              console.log(reply);
+          });
+        console.log("__________________________entraron! :D__________________________________")
     });
     }
-    console.log("palabras pusheadas por trend");
-    console.log(palabras.join(","));
-    return palabras;
+    //console.log("palabras pusheadas por trend");
+    //palabrasbienformadas=palabras.join(",");
+    //console.log(palabrasbienformadas);
+    //console.log("IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+    //return palabrasbienformadas;
 
 
 }
@@ -119,24 +130,42 @@ globalStream.destroy();
 
     woeids=[1,753692,754542,766273,774508];
 
-    trendsString=trackTrends(woeids);
+    trackTrends(woeids);
 
-    console.log("palabras Recogidas:"+trendsString.join(","));
+    //console.log("palabras Recogidas:"+trendsString.join(","));
     console.info("empezando nuevo stream despues de los tags...");
     console.log("esto es el string de hashes...");
-    console.log(trendsString.join(","));
-    connectStream(trendsString.join(","),db);
+  //  console.log(trendsString);
+    client.get('trends', function(err, reply) {
+        console.log(reply);
+        connectStream(reply,db);
+    });
 
-}, 25*1000);
+}, 15*60*1000);
+/*
+*
+*  773418
+ A Corunha : 763246
+ León : 765099
+ Santander : 773964
+*
+* */
+    woeids=[1,753692,754542,766273,774508,773418,763246,765099,773964];
 
-    woeids=[1,753692,754542,766273,774508];
 
-    trendsString=trackTrends(woeids);
+    client.on('connect', function() {
+        console.log('connected');
+        trackTrends(woeids);
+    });
+
+    client.get('trends', function(err, reply) {
+        console.log(reply);
+        connectStream(reply,db);
+    });
+
     console.info("empezando el primer stream");
-    console.log(trendsString.join(","));
 
-    console.log("_______________________________________________________________")
-connectStream(trendsString.join(","),db);
+
 
 
 });
--- Daemon/nodestream/keys.js
+++ Daemon/nodestream/keys.js
@@ -1,25 +1,22 @@
 /**
  * Created by Treball on 21/07/2015.
  */
-/*var keys={
+var keys={
     "consumer_key":"sEKnw0eg7swg8n63oS2ZL9Stp",
     "consumer_secret":"CvHzQMcUhd0NrGzwfVjZyKArOQ5i8ss1hLzXEw7v1DtVyKlJZC",
     "access_token_key":"872376888-hAFTE0d45sC4oP6WadEd5IveBeYL6fR4GRLxIrpI",
     "access_token_secret":"F3a8HlytFwgXaLX6LgAPMvfASvfqLmHsjAP1BgWKipDlw",
-}*/
-
+}
 /*var keys={
     "consumer_key":"qQjOWiaFWQwHNvuz29evUltzN",
     "consumer_secret":"Xw89udDKTbSSGw3fAVO5lcguxxxPiJ9hQeXLcUMmkMwzb4Xc1Y",
     "access_token_key":"872376888-1j6WeadVnpY4qBhPXGcqNd0P5SEHsud5tlQ2FJyk",
     "access_token_secret":"lOoUbqdsCzitYuYz4FRCg8uIwKOXtbddLyRPWpDCm3F2j",
 }*/
-
-
-var keys={
+/*var keys={
     "consumer_key":"fG4ee7iF2HtTH76aNw3pKStJv",
     "consumer_secret":"Z0VXx7aP4Uh5XEqsIWeN44q9I53s34jEnMTQliFqQswyvgvuo2",
     "access_token_key":"872376888-UuN9tL0op6cKx477ntSawRsKU1Xz8N85sqE0D45x",
     "access_token_secret":"5F9ZMdWL9I5uanRfMPJgtcup5XiR7WUoyjgAwDlYyYsOW",
-}
+}*/
 exports.keys = keys;
